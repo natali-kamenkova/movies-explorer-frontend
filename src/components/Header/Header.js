dd64d4254ccab9acd2c55893203376281f5310db
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-27 20:53:04 +0300

    burger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,76 @@
+import React from "react";
+import {useState} from "react";
+import "./Header.css";
+import logo from "../../images/logo.svg";
+import profile from "../../images/profile.svg";
+import {Link, useLocation} from "react-router-dom";
 
-import logo from '../../images/logo.svg';
-import { Switch, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+function Header() {
+    const [openNavigation, setIsOpenBurgerMenu] = useState(false);
 
+    const location = useLocation();
+    const headerClassName = `header ${location.pathname === "/" ? "header_color_emerald" : "header_color_dark"}`;
 
-function Header(props) {
+    function handleBurgerMenuButton() {
+        setIsOpenBurgerMenu(!openNavigation);
+    }
 
-  return (
-    <header className="header">
-      <img
-        className="header__logo"
-        src={logo}
-        alt="Логотип"
-      />
-      <div className='header__links'>
-        <Switch>
-          <Route exact path='/sign-in'>
-            <Link to="/sign-up" className='header__link-singup'>Регистрация</Link>
-          </Route>
-          <Route exact path='/sign-up'>
-            <Link to="/sign-in" className='header__link-singup'>Войти</Link>
-          </Route>
-          <Route exact path='/'>
-            <div>
-              <Link to="/email" className='header__link-singup'>{props.email}</Link>
-              <button className="header__enter" onClick={props.onLogout}>Выйти</button>
+    return (
+        <header className={headerClassName}>
+            <div className="header__container">
+                <Link to="/">
+                    <img className="logo" src={logo} alt="Логотоп"/>
+                </Link>
+                <div className="header__links">
+                    {location.pathname === "/" ? ( // main
+                        <>
+                            <Link
+                                to="/signup"
+                                className="header__link header__link_color_white"
+                            >
+                                Регистрация
+                            </Link>
+                            <Link
+                                to="/signin"
+                                className="header__link header__link_color_green"
+                            >
+                                Войти
+                            </Link>
+                        </> // others
+                    ) : (
+                        <>
+                            <Link
+                                to="/movies"
+                                className="header__link header__link_color_white header__link_hidden"
+                            >
+                                Фильмы
+                            </Link>
+                            <Link
+                                to="/saved-movies"
+                                className="header__link header__link_right header__link_color_white header__link_hidden"
+                            >
+                                Сохранённые фильмы
+                            </Link>
+                            <Link to="/profile" className="header__link">
+                                <img
+                                    className="header__link-image header__link_hidden"
+                                    src={profile}
+                                    alt="Профиль"
+                                />
+                            </Link>
+                            <button
+                                aria-label="Open navigation"
+                                className="header__burger-menu"
+                                type="button"
+                                onClick={handleBurgerMenuButton}
+                            />
+                        </>
+                    )}
+                </div>
             </div>
-          </Route>
-
-        </Switch>
-
-
-      </div>
-    </header>
-
-  )
-
+            {openNavigation ? (<Navigation handleBurgerMenuButton={handleBurgerMenuButton}/>) : ("")}
+        </header>
+    );
 }
+
 export default Header;
-
-
