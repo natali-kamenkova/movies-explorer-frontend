32a1c3d3ba608132cbd14e840b0d97f106259557
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-27 20:40:34 +0300

    f

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-  return (
-    <footer className="footer container">
-      <p className="footer__text">© 2020 Mesto Russia</p>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <p className="footer__text">
+                Учебный проект Яндекс.Практикум х BeatFilm.
+            </p>
+            <div className="footer__info">
+                <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+                <nav>
+                    <ul className="footer__links">
+                        <li>
+                            <a
+                                className="footer__link"
+                                href="https://practicum.yandex.ru"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Яндекс.Практикум
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="footer__link"
+                                href="https://github.com/natali-kamenkova"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
