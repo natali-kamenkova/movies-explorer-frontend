25734af976ee3f5e6f2388511acd6398d00224d3
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-30 23:09:03 +0300

    p

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,68 +10,63 @@ function Header() {
     const [openNavigation, setIsOpenBurgerMenu] = useState(false);
 
     const location = useLocation();
-    const headerClassName = `header ${location.pathname === "/" ? "header_color_emerald" : "header_color_dark"}`;
 
     function handleBurgerMenuButton() {
         setIsOpenBurgerMenu(!openNavigation);
     }
 
-    return (
-        <header className={headerClassName}>
-            <div className="header__container">
-                <Link to="/">
-                    <img className="logo" src={logo} alt="Логотип"/>
-                </Link>
-                <div className="header__links">
-                    {location.pathname === "/" ? ( // main
-                        <>
-                            <Link
-                                to="/signup"
-                                className="header__link header__link_color_white"
-                            >
-                                Регистрация
-                            </Link>
-                            <Link
-                                to="/signin"
-                                className="header__link header__link_color_green"
-                            >
-                                Войти
-                            </Link>
-                        </> // others
-                    ) : (
-                        <>
-                            <Link
-                                to="/movies"
-                                className="header__link header__link_color_white header__link_hidden"
-                            >
-                                Фильмы
-                            </Link>
-                            <Link
-                                to="/saved-movies"
-                                className="header__link header__link_right header__link_color_white header__link_hidden"
-                            >
-                                Сохранённые фильмы
-                            </Link>
-                            <Link to="/profile" className="header__link">
-                                <img
-                                    className="header__link-image header__link_hidden"
-                                    src={profile}
-                                    alt="Профиль"
-                                />
-                            </Link>
-                            <button
-                                aria-label="Open navigation"
-                                className="header__burger-menu"
-                                type="button"
-                                onClick={handleBurgerMenuButton}
-                            />
-                        </>
-                    )}
-                </div>
+    return (<header className={"header header_color"}>
+        <div className="header__container">
+            <Link to="/">
+                <img className="logo" src={logo} alt="Логотип"/>
+            </Link>
+            <div className="header__links">
+                {location.pathname === "/" ? ( // main
+                    <>
+                        <Link
+                            to="/signup"
+                            className="header__link header__link_color_white"
+                        >
+                            Регистрация
+                        </Link>
+                        <Link
+                            to="/signin"
+                            className="header__link header__link_color_green"
+                        >
+                            Войти
+                        </Link>
+                    </> // others
+                ) : (<>
+                    <Link
+                        to="/movies"
+                        className="header__link header__link_color_white header__link_hidden"
+                    >
+                        Фильмы
+                    </Link>
+                    <Link
+                        to="/saved-movies"
+                        className="header__link header__link_right header__link_color_white header__link_hidden"
+                    >
+                        Сохранённые фильмы
+                    </Link>
+                    <Link to="/profile" className="header__link">
+                        <img
+                            className="header__link-image header__link_hidden"
+                            src={profile}
+                            alt="Профиль"
+                        />
+                    </Link>
+                    <button
+                        aria-label="Open navigation"
+                        className="header__burger-menu"
+                        type="button"
+                        onClick={handleBurgerMenuButton}
+                    />
+                </>)}
             </div>
-            {openNavigation ? (<Navigation handleBurgerMenuButton={handleBurgerMenuButton}/>) : ("")}
-        </header>
-    );
+        </div>
+        {openNavigation ? (<Navigation handleBurgerMenuButton={handleBurgerMenuButton}/>) : ("")}
+    </header>);
 }
 
 export default Header;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,11 @@ import React from "react";
 import AboutMe from "../AboutMe/AboutMe";
 import AboutProject from "../AboutProject/AboutProject";
 import "./Main.css";
+import Promo from "../Promo/Promo";
 
 function Main() {
     return (<main className="main">
+        <Promo/>
         <AboutProject/>
         <AboutMe/>
     </main>);
