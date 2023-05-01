fae1274fce9574c83b884c941cdab1200200db9e
Natali Kamenkova <natalikamenkova@gmail.com>
2023-05-01 22:54:17 +0300

    techologies

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,14 @@ import AboutMe from "../AboutMe/AboutMe";
 import AboutProject from "../AboutProject/AboutProject";
 import "./Main.css";
 import Promo from "../Promo/Promo";
+import Techs from "../Techs/Techs";
 
 function Main() {
     return (<main className="main">
         <Promo/>
         <AboutProject/>
         <AboutMe/>
+        <Techs />
     </main>);
 }
 
