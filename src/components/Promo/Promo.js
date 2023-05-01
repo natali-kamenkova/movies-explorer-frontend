b43bb41d29cd06f58fe32095f2248be4a2ee601a
Natali Kamenkova <natalikamenkova@gmail.com>
2023-05-01 22:28:21 +0300

    Promo.js

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Promo.css";
+import promo from "../../images/promo.svg";
 
 function Promo() {
     return (<section className="promo">
         <h1 className="promo__text">
             Учебный проект студента факультета Веб-разработки.
         </h1>
+        <img className="promo__logo" src={promo} alt="Логотип"/>
     </section>);
 }
 
