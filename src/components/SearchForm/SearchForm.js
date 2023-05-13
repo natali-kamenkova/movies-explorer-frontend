0c82d5322662f467f0fbc36386d3dd7f4f6c9dbc
Natali Kamenkova <natalikamenkova@gmail.com>
2023-05-13 23:01:30 +0300

    поиск

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,24 +1,90 @@
-import React from "react";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import "./SearchForm.css";
 
-function SearchForm() {
-  return (
-    <div className="search-form">
-      <form name="search-form" className="search-form__form">
-        <input
-          className="search-form__input"
-          type="text"
-          placeholder="Фильм"
-          required
-        />
-        <button className="search-form__button" type="submit">
-          Поиск
-        </button>
-      </form>
-      <FilterCheckbox />
-    </div>
-  );
+function SearchForm({handleSearch, setPreloader, isLoading, errorMessage}) {
+    const submitButtonClassName = `search-form__button ${
+        isLoading ? "search-form__button_disabled" : ""
+    }`;
+    const inputClassName = `search-form__input ${
+        isLoading ? "search-form__input_disabled" : ""
+    }`;
+
+    const location = useLocation();
+
+    const [keyword, setKeyword] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
+
+    useEffect(() => {
+        if (location.pathname === "/movies") {
+            const keyword = localStorage.getItem("keyword");
+            const checked = localStorage.getItem("isChecked");
+            if (keyword) {
+                setKeyword(keyword);
+            }
+            if (JSON.parse(checked) === true) {
+                setIsChecked(true);
+            } else {
+                setIsChecked(false);
+            }
+        }
+    }, [location.pathname]);
+
+    function toggleCheckbox(isChecked) {
+        setIsChecked(isChecked);
+        handleSearch(keyword, isChecked);
+    }
+
+    function handleCheckbox(evt) {
+        toggleCheckbox(evt.target.checked);
+    }
+
+    function handleKeyword(evt) {
+        setKeyword(evt.target.value);
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        handleSearch(keyword, isChecked);
+        setPreloader(true);
+    }
+
+    return (
+        <div className="search-form">
+            <form
+                name="search-form"
+                className="search-form__form"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    className={inputClassName}
+                    type="text"
+                    name="keyword"
+                    id="keyword"
+                    placeholder="Введите ключевое слово для поиска"
+                    value={keyword}
+                    onChange={handleKeyword}
+                    disabled={isLoading}
+                />
+                <button
+                    className={submitButtonClassName}
+                    type="submit"
+                    disabled={isLoading}
+                >
+                    Поиск
+                </button>
+            </form>
+            <span className="search-form__error" id="keywords-error">
+        {errorMessage}
+      </span>
+            <FilterCheckbox
+                onCheckboxToggle={handleCheckbox}
+                isLoading={isLoading}
+                isChecked={isChecked}
+            />
+        </div>
+    );
 }
 
 export default SearchForm;
